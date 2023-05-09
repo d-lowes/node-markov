ebc6b61873c816fc766c19f7ac400ef2e6fd31d1
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-05-09 16:13:36 -0700

    is the cat in the hat?

--- markov.js
+++ markov.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /** Textual markov chain generator. */
 
 
@@ -7,7 +9,7 @@ class MarkovMachine {
 
   constructor(text) {
     // A "word" will also include any punctuation around the word, so this will
-    // include things like "The", "cat", "cat.".
+    // include things like ["The", "cat", "cat."].
     this.words = text.split(/[ \r\n]+/);
     this.chains = this.getChains();
   }
@@ -15,7 +17,7 @@ class MarkovMachine {
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +25,24 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
+    let chains = {};
+
+    for (let i = 0; i < this.words.length; i++) {
+      let current = this.words[i];
+      let next = this.words[i + 1] || null;
+
+      if (chains[current]) {
+        chains[current].push(next);
+      } else {
+        chains[current] = [next];
+      }
+    }
+
+    return chains;
   }
 
 
@@ -40,5 +55,39 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    const chains = this.chains;
+
+    let randomText = `${this.words[0]} `;
+    let currentWord = this.words[0];
+
+    console.log("current word =", currentWord);
+    console.log("word array =", this.words);
+
+    while (currentWord != null) {
+      currentWord = MarkovMachine.randomWord(chains[currentWord])
+
+      if (currentWord != null) {
+        randomText += `${currentWord} `;
+      }
+    }
+
+    console.log("random text =", randomText);
+    return randomText.slice(0, -1);
+  }
+  /** Return a random word from the array of words */
+  static randomWord(words) {
+    return words[Math.floor(Math.random() * words.length)];
   }
 }
+
+
+/** Export the class */
+
+module.exports = {
+  MarkovMachine,
+};
+
+let machine = new MarkovMachine("The cat is in the hat. The cat is the cat. The hat is a cat.");
+
+machine.getText();
